85d0e0b029da71cafe42c394682965538a79da44
Pau Minguet <pminmic@gmail.com>
2025-06-20 03:24:48 +0200

    feat: Clase Publicacion padre

--- src/com/biblioteca/model/Publicacion.java
+++ src/com/biblioteca/model/Publicacion.java
@@ -1,1 +1,49 @@
 package com.biblioteca.model;
+
+import java.time.LocalDate;
+
+public abstract class Publicacion {
+    private String titulo;
+    private int ejemplares;
+    private String identificador;
+    private LocalDate fechaPublicacion;
+
+    public Publicacion(String t, int ejem, String id, LocalDate fP) {
+        this.titulo = t;
+        this.ejemplares = ejem;
+        this.identificador = id;
+        this.fechaPublicacion = fP;
+    }
+
+    public void setTitulo(String t) {
+        this.titulo = t;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void addEjemplares(int add) {
+        this.ejemplares += add;
+    }
+
+    public int getEjemplares() {
+        return this.ejemplares;
+    }
+
+    public void setIdentificador(String id) {
+        this.identificador = id;
+    }
+
+    public String getIdentificador() {
+        return this.identificador;
+    }
+
+    public void setFechaPublicacion(LocalDate fP) {
+        this.fechaPublicacion = fP;
+    }
+
+    public LocalDate getFechaPublicacion() {
+        return this.fechaPublicacion;
+    }
+}
